277b124d4054b6ce556a18fe1371d98a2039a105
br <penacarlos136@gmail.com>
2026-02-02 12:44:17 -0500

    Updated with new proyects as of 30/1

---
+++ gah/matriz1.py
@@ -0,0 +1,13 @@
+A = [[18,17,20,16,17],
+     [5,10,20,19,17],
+     [12,14,150,19,9],
+     [18,20,12,11,10]]
+B =["Carlos",
+    "Letizia",
+    "Flavia",
+    "Alejandro"]
+print(B[2])
+unaColumna = []
+for i in range(0,len(A)):
+    unaColumna.append(A[i][3])
+print(unaColumna)
