ef217a72004d9b7ed28d1c11ceecb50a859f6b94
carlos <penacarlos136@gmail.com>
2026-08-13 10:46:21 -0500

    slow aa

---
+++ gah2-ep2/1berjecicio.py
@@ -0,0 +1,7 @@
+pi = 3.145
+radio = float(input("Ingrese un numero: "))
+area = 4 * pi * radio**2
+volumen = 4 / 3 * pi * radio**3
+
+print("El area:", round(area,3))
+print("El volumen:", round(volumen,3))
---
+++ gah2-ep2/kain.py
@@ -0,0 +1,50 @@
+from random import random
+from xml.etree.ElementTree import tostring
+
+# print ("Hola mundo")
+#
+# nombre = input("Ingresa tu nombre:")
+#
+# print("Hola " + nombre)
+#
+# edad = int (input("Ingresa tu edad: "))
+# print("Tienes " + str(edad) + " años")
+# #equvale a print("tienes ",edad," años")
+# print("En 10 años tendras: " + str(edad + 10) + " años")
+
+# # int
+# num = 5670
+# num2 = 0b1011
+# print (num2, bin(num2))
+#
+# peso = 45.67
+# peso = float(input("Ingresa tu peso: "))
+# print(peso)
+#
+# #bool
+#
+# bandera = True
+# if bandera:
+#     print("ASS")
+#
+# es_mayor = peso > 80
+# if es_mayor:
+#     print("  ... que pesao")
+
+#cadena
+cad_1 = "ja"
+cad_2 = "to"
+print(cad_1)
+print(cad_2)
+cad_l = "dont\t dead \n open \t inside"
+print(cad_l)
+#operadores
+print(23+7)
+print(23-7)
+print(23*7)
+print(23/7)
+print(23//7) # div entero, el cociente
+print(23%7)  # modulo, el residuo
+print(23**7) # potencia
+
+
