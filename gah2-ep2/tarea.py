84504bb4dc632bdce5b3d9a748561926e7b161fd
kenny <penacarlos136@gmail.com>
2026-08-17 23:15:52 -0500

    tarea echa

--- gah2-ep2/tarea.py
+++ gah2-ep2/tarea.py
@@ -1,4 +1,25 @@
 # ejercicio 2
-sol = int(input("Ingrese un monto: "))
-dolar = 3.350
-print("Tienes el equivalente de ",round((sol / dolar),2)," dolares")
+# sol = int(input("Ingrese un monto: "))
+# dolar = 3.350
+# print("Tienes el equivalente de ",round((sol / dolar),2)," dolares")
+# # ejercicio 3
+# nombre = input("Ingrese su nombre: ")
+edad = int(input("Ingrese su edad: "))
+print("Cumplira 100 años en: ",2126-edad )
+# ejercicio 4
+minuto = int(input("Ingresa tiempo en minutos: "))
+print("En segundos: ",minuto*60)
+
+# ejercicio 5
+
+segundos = int(input("Ingresa tiempo en segundos: "))
+
+dias = segundos // (60 * 60 * 24)
+segundos = segundos % (60 * 60 * 24)
+#resta
+horas = segundos // (60 * 60)
+segundos = segundos % (60 * 60)
+#resta
+minutos = segundos // 60
+segundos = segundos % 60
+print(f"{dias}:{horas}:{minutos}:{segundos}")
