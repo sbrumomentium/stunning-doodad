c729c1a169fc7fecaa20376721a3b6f20010be98
carlos <penacarlos136@gmail.com>
2026-09-07 07:04:20 -0500

    tarea lab 1

---
+++ gah2-ep2/tarea_lab_1_submission.py
@@ -0,0 +1,39 @@
+from math import pi
+
+
+def pregunta_1(radio: float, angulo: float) -> float:
+
+    return 0
+
+
+def pregunta_2(vx: float, vy: float, vz: float) -> float:
+    """
+    Parametros:
+        vx (float):  Es la coordenada en x
+        vy (float):  Es la coordenada en y
+        vz (float):  Es la coordenada en z
+    Retorna:
+        float : es el modulo
+    """
+    return 0
+
+
+def pregunta_3(edad: int) -> str:
+    """
+    Parametros:
+        edad (int): Edad de la persona.
+    Retorna:
+        str: Clasificacion de la edad.
+    """
+    return ""
+
+
+def pregunta_4(peso: int, altura: float) -> str:
+    """
+    Parametros:
+      peso (float): Peso en kilogramos (kg).
+      altura (float): Altura en metros (m).
+    Retorna:
+        str : La categoria del IMC segun la OMS.
+    """
+    return ""
